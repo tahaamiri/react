c993aee9f88ed237ea735e4956c4f78e8c4a7289
amiriTaha <amiri.taha.98@gmail.com>
2021-12-27 11:30:38 +0330

    Using axios to get response

--- react-node-app/client/src/App.tsx
+++ react-node-app/client/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState<any>("");
+
+
+	useEffect(() => {
+		axios
+			.get("/api")
+			.then((response) => {
+				console.log(response);
+                setMessage(response.data.message);
+                
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>{!message ? "Loading..." : message}</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
